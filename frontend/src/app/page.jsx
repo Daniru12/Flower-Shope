e94dc0b861168b76e350db9e550bc42800fb9a2c
ishani2924 <167461216+ishani2924@users.noreply.github.com>
2025-07-18 01:54:09 +0530

    component 2,3

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Header from './components/Header';
 import HeroSection from './Components/HeroSection'
+import { SpecialOfferCard } from "./Components/specialOffers";
+import { PromotionCard } from "./Components/PromotionCard";
 
 export default function Home() {
   return (
     <>
     
       <HeroSection />
+      <SpecialOfferCard/>
+      <PromotionCard/>
     </>
   )
 }
